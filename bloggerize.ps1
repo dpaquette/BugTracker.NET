1bca2bfb28f5923d987698b5f08ae7f0a88f8b5d
Simon Timms <stimms@gmail.com>
2014-08-26 07:56:21 -0600

    Removing dates

--- bloggerize.ps1
+++ bloggerize.ps1
@@ -11,6 +11,7 @@ Get-ChildItem "Blog *md" | ForEach-Object {
 	$header += "title:  ""$($blogName)""`n"
 	$header += "date:   $($startingDate.AddDays($counter).ToString("yyyy-MM-dd"))`n"
 	$header += "categories: asp.net`n"
+	$header += "author: David Paquette and Simon Timms`n"
 	$header += "---`n"
 	del "blog/_posts/$($startingDate.ToString("yyyy-MM-dd"))-$($blogName).md"
 	Add-Content -path "blog/_posts/$($startingDate.ToString("yyyy-MM-dd"))-$($blogName).md" -value $header
