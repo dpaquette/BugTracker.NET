3e3934341e3cf3bbe9cda59ecf2e53efaf349e2f
Simon Timms <stimms@gmail.com>
2014-12-16 17:40:26 -0700

    Adding new master page for not logged in users

--- src/BugTracker.Web/LoggedOut.Master.cs
+++ src/BugTracker.Web/LoggedOut.Master.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Linq;
-using btnet.Controls;
 using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace btnet
 {
-    public partial class LoggedIn : System.Web.UI.MasterPage
+    public partial class LoggedOut : System.Web.UI.MasterPage
     {
-
-        public MainMenu Menu { get { return this.MainMenu; } }
-
         protected void Page_Load(object sender, EventArgs e)
         {
 
