f370c0275bbe7d083b1237e61991732ffcd56c5c
David Paquette <david.paquette@usask.ca>
2015-03-17 12:20:14 -0500

    Updated statuses grid to use DataTables.net

--- src/BugTracker.Web/statuses.aspx.cs
+++ src/BugTracker.Web/statuses.aspx.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
+using btnet.Models;
 using btnet.Security;
 
 namespace btnet
@@ -7,29 +10,23 @@ namespace btnet
     [PageAuthorize(BtnetRoles.Admin)]
     public partial class statuses : BasePage
     {
-        protected DataSet ds;
+        protected IEnumerable<Status> Statuses;
 
         protected void Page_Load(Object sender, EventArgs e)
         {
 
             Util.do_not_cache(Response);
-
+            Master.Menu.SelectedItem = "admin";
 
             Page.Header.Title = Util.get_setting("AppTitle", "BugTracker.NET") + " - "
                 + "statuses";
 
-            ds = btnet.DbUtil.get_dataset(
-                new SQLString(@"select st_id [id],
-		        st_name [status],
-		        st_sort_seq [sort seq],
-		        st_style [css<br>class],
-		        case when st_default = 1 then 'Y' else 'N' end [default],
-		        st_id [hidden]
-		        from statuses order by st_sort_seq"));
+            using (Context context = new Context())
+            {
+                Statuses = context.Statuses.OrderBy(s => s.SortOrder).ToList();
+            }
 
         }
-
-
 
     }
 }
