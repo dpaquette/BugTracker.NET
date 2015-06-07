a33da4140a8a0c7e99a1212a116835141c3d2a40
David Paquette <david.paquette@usask.ca>
2015-06-07 12:08:31 -0500

    Add BugFromEmailController

---
+++ src/BugTracker.Web/Controllers/BugFromEmailController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace btnet.Controllers
+{
+    [Authorize]
+    public class BugFromEmailController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post()
+        {            
+            return Ok();
+        }
+    }
+}
