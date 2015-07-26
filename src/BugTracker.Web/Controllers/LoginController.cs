97e0ee6e71bb11c8ac8df9a0dd89290bebbc3fc2
David Paquette <david.paquette@usask.ca>
2015-07-26 15:44:27 -0500

    Refactored login logic to support login from other clients

---
+++ src/BugTracker.Web/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using btnet.Security;
+
+namespace btnet.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public IHttpActionResult Login(string user, string password)
+        {
+            LoginResult loginResult = Authenticate.AttemptLogin(Request.GetOwinContext(), user, password);
+
+            if (loginResult.Success)
+            {
+                return Ok();
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.Forbidden);
+            }
+        }
+    }
+}
---
+++ src/BugTracker.Web/Security/LoginResult.cs
@@ -0,0 +1,9 @@
+﻿namespace btnet.Security
+{
+    public class LoginResult
+    {
+        public bool Success { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
